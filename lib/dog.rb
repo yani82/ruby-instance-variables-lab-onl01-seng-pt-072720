d86b177120bc083643e8ca768278c75f37fd06b2
Yani Asari <yani.iman1@gmail.com>
2020-08-09 20:54:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,15 @@
 class Dog
   def name=(dog_name)
     this_dogs_name = dog_name
-    lassie = Dog.new
-lassie.name=("Lassie")
-lassie.name
-  end
  end
  
   def name
     this_dogs_name
   end
+  
+  lassie = Dog.new
+lassie.name=("Lassie")
+lassie.name
+  end
+  
 end
