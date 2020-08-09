68cd4eb46cb3c21df844f677d6e6d23c401c858d
Yani Asari <yani.iman1@gmail.com>
2020-08-09 20:57:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,12 +5,13 @@ dogs_name
  end
  
   def name
-    this_dogs_name
+    @this_dogs_name
   end
+end
   
-  lassie = Dog.new
-lassie.name=("Lassie")
-lassie.name
-  end
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
   
 end
